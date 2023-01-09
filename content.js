6c4301d21380a17eb28c2fdaaad617fd9002f040
Cao Duc Anh Dung <dung.cda@teko.vn>
2023-01-09 14:52:01 +0700

    update subid, masterlink, remove params

--- content.js
+++ content.js
@@ -1,34 +1,44 @@
 console.log("Waiting for event ...")
 
 chrome.runtime.onMessage.addListener((request, _, sendResponse) => {
-    run(request, sendResponse)
+    run(request.originUrls, request.subId1, request.subId2, request.subId3, request.masterLink, sendResponse)
     return true
 });
 
-async function run(originUrls, sendResponse) {
+async function run(originUrls, subId1, subId2, subId3, masterLink, sendResponse) {
     console.log("received origin urls", originUrls)
     const affLinks = []
-    setMasterLink()
+
     for (let i = 0; i < originUrls.length; i++) {
+        setMasterLink(masterLink)
+        setSubIds(subId1, subId2, subId3)
         setURL(originUrls[i])
         clickSubmit()
-        await sleep(300)
+        await sleep(500)
         affLinks.push(getAffLink())
     }
 
+    await sleep(500)
     sendResponse(affLinks)
+    return true
 }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function setMasterLink() {
-    document.getElementById("masterLink").value = "https://c.lazada.vn/t/c.0kjvGX"
+function setMasterLink(link) {
+    document.getElementById("masterLink").value = link
 }
 
 function setURL(url) {
     document.getElementById("sourceUrl").value = url
+}
+
+function setSubIds(subId1, subId2, subId3) {
+    document.getElementById("subId1").value = subId1
+    document.getElementById("subId2").value = subId2
+    document.getElementById("subId3").value = subId3
 }
 
 function clickSubmit() {
--- popup.js
+++ popup.js
@@ -12,6 +12,19 @@ async function process() {
 
 
     let content = document.getElementById("inp").value
+    let subId1 = document.getElementById("subId1").value
+    let subId2 = document.getElementById("subId2").value
+    let subId3 = document.getElementById("subId3").value
+    let userCode = document.getElementById("userCode").value
+
+
+    const out = document.getElementById("out")
+
+    let masterLink = await getMasterLink(userCode)
+    if (masterLink == '') {
+        setOutput(out, "Lỗi lấy master link")
+        return
+    }
 
     let urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g
 
@@ -22,28 +35,41 @@ async function process() {
 
     let urls = content.match(urlRegex)
 
+
     const originUrls = []
+
     for (let i = 0; i < urls.length; i++) {
         let url = await openTabAndGetUrl(urls[i])
+        if (url == '') {
+            setOutput(out, 'Lỗi khi xử  lý link: ' + urls[i] + ' vui lòng kiểm tra kết nối mạng và thử lại')
+            return
+        }
         originUrls.push(url)
     }
 
     console.log("originUrls", originUrls)
 
-    const out = document.getElementById("out")
-
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, originUrls, (response) => {
-            console.log("affLinks", response)
+        chrome.tabs.sendMessage(
+            activeTab.id,
+            {
+                originUrls: originUrls,
+                subId1: subId1,
+                subId2: subId2,
+                subId3: subId3,
+                masterLink: masterLink
+            },
+            (response) => {
+                console.log("affLinks", response)
 
-            for (let i = 0; i < urls.length; i++) {
-                content = content.replace(urls[i], response[i])
-            }
+                for (let i = 0; i < urls.length; i++) {
+                    content = content.replace(urls[i], response[i])
+                }
 
-            out.value = content
-            out.hidden = false
-        });
+                setOutput(out, content)
+                subBtn.innerText = 'Đã xong'
+            });
     });
 }
 
@@ -54,17 +80,59 @@ async function openTabAndGetUrl(url) {
         tabID = tab.id
     });
 
-    await sleep(600)
+    // waiting for chrome to open tab
+    await sleep(500)
+    if (tabID == 0) {
+        console.log("error tabID = 0", url)
+        return ''
+    }
 
     chrome.tabs.get(tabID, function (tab) {
         res = (tab.pendingUrl || tab.url)
     })
 
-    await sleep(600)
+    await sleep(1000)
     chrome.tabs.remove(tabID)
+    res = removeURLParameter(res, "trafficFrom")
+    res = removeURLParameter(res, "laz_trackid")
+    res = removeURLParameter(res, "mkttid")
     return res
 }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function removeURLParameter(url, parameter) {
+    //prefer to use l.search if you have a location/link object
+    var urlparts = url.split('?');
+    if (urlparts.length >= 2) {
+
+        var prefix = encodeURIComponent(parameter) + '=';
+        var pars = urlparts[1].split(/[&;]/g);
+
+        //reverse iteration as may be destructive
+        for (var i = pars.length; i-- > 0;) {
+            //idiom for string.startsWith
+            if (pars[i].lastIndexOf(prefix, 0) !== -1) {
+                pars.splice(i, 1);
+            }
+        }
+
+        return urlparts[0] + (pars.length > 0 ? '?' + pars.join('&') : '');
+    }
+    return url;
+}
+
+function setOutput(output, content) {
+    output.value = content
+    output.hidden = false
+}
+
+
+async function getMasterLink(userCode) {
+    const response = await fetch('https://api.vouchertoday.org/api/getMasterLink?code=' + userCode);
+    const res = await response.json()
+    console.log(res.link)
+    return res.link
 }
